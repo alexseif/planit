97c880dd1abdc80b8b4aed48051b2e75bc5faae0
Alex Seif <alex.seif@gmail.com>
2023-03-14 08:52:08 +0200

    Goal UI and types

--- src/Controller/GoalController.php
+++ src/Controller/GoalController.php
@@ -15,21 +15,27 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class GoalController extends AbstractController
 {
+
     /**
      * @Route("/", name="app_goal_index", methods={"GET"})
      */
     public function index(GoalRepository $goalRepository): Response
     {
         return $this->render('goal/index.html.twig', [
-            'goals' => $goalRepository->findAll(),
+          'goals' => [
+            'personal' => $goalRepository->findBy(['professional' => false]),
+            'professional' => $goalRepository->findBy(['professional' => true]),
+          ],
         ]);
     }
 
     /**
      * @Route("/new", name="app_goal_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, GoalRepository $goalRepository): Response
-    {
+    public function new(
+      Request $request,
+      GoalRepository $goalRepository
+    ): Response {
         $goal = new Goal();
         $form = $this->createForm(GoalType::class, $goal);
         $form->handleRequest($request);
@@ -37,12 +43,16 @@ class GoalController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $goalRepository->add($goal, true);
 
-            return $this->redirectToRoute('app_goal_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute(
+              'app_goal_index',
+              [],
+              Response::HTTP_SEE_OTHER
+            );
         }
 
-        return $this->renderForm('goal/new.html.twig', [
-            'goal' => $goal,
-            'form' => $form,
+        return $this->renderForm('goal / new.html . twig', [
+          'goal' => $goal,
+          'form' => $form,
         ]);
     }
 
@@ -51,40 +61,58 @@ class GoalController extends AbstractController
      */
     public function show(Goal $goal): Response
     {
-        return $this->render('goal/show.html.twig', [
-            'goal' => $goal,
+        return $this->render('goal / show . html . twig', [
+          'goal' => $goal,
         ]);
     }
 
     /**
      * @Route("/{id}/edit", name="app_goal_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, Goal $goal, GoalRepository $goalRepository): Response
-    {
+    public function edit(
+      Request $request,
+      Goal $goal,
+      GoalRepository $goalRepository
+    ): Response {
         $form = $this->createForm(GoalType::class, $goal);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $goalRepository->add($goal, true);
 
-            return $this->redirectToRoute('app_goal_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute(
+              'app_goal_index',
+              [],
+              Response::HTTP_SEE_OTHER
+            );
         }
 
-        return $this->renderForm('goal/edit.html.twig', [
-            'goal' => $goal,
-            'form' => $form,
+        return $this->renderForm('goal / edit . html . twig', [
+          'goal' => $goal,
+          'form' => $form,
         ]);
     }
 
     /**
      * @Route("/{id}", name="app_goal_delete", methods={"POST"})
      */
-    public function delete(Request $request, Goal $goal, GoalRepository $goalRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$goal->getId(), $request->request->get('_token'))) {
+    public function delete(
+      Request $request,
+      Goal $goal,
+      GoalRepository $goalRepository
+    ): Response {
+        if ($this->isCsrfTokenValid(
+          'delete' . $goal->getId(),
+          $request->request->get('_token')
+        )) {
             $goalRepository->remove($goal, true);
         }
 
-        return $this->redirectToRoute('app_goal_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute(
+          'app_goal_index',
+          [],
+          Response::HTTP_SEE_OTHER
+        );
     }
+
 }
